28ab4d251ad84af22a8285639b00e6cc326339eb
Ravindu Hasanka <rhatu2000@gmail.com>
2023-08-07 10:20:43 +0530

    question 03 and 04

--- q1.js
+++ q1.js
@@ -13,13 +13,17 @@ var web = require('http');
 web.createServer(function(req,res){
   res.writeHead(200, {'Content-Type': 'text/html'});
   // res.write("Question 01 : "+sum(5,4));
-  // Q2();
   res.end("Check console to view answers");
+  Q1();
+  Q2();
+  Q3();
+  Q4();
+  
 
 }).listen(8080)
 
 function Q1(){
-  console.log("Question 01 Answer: "+Q1()+"\n");
+  console.log("Question 01 Answer: "+sum(6,9)+"\n");
 }
 
 function sum(a,b){
@@ -31,5 +35,40 @@ function Q2(){
   console.log("Question 2 Answers\n");
   for(const i of arr){
     console.log(`${i*i}`);
+  }
+  console.log("\n");
+}
+
+function Q3(){
+  console.log("Question 03 Answer: "+findMax());
+}
+function findMax(){
+  var arr = [61,25,32,4,63,8];
+  if (arr.length>1) {
+    var temp = arr[0];
+    for(var i in arr){
+      temp = Math.max(temp,i);
+    }
+    return temp;
+  }
+  if(arr.length==1){
+    return arr[0];
+  }
+  else{
+    return 0;
+  }
+  
+}
+
+function Q4(){
+  console.log("Question 04 Answer: "+factorial(6)+"\n");
+}
+
+function factorial(x){
+  if(x==1){
+    return 1;
+  }
+  else{
+    return x*factorial(x-1);
   }
 }
